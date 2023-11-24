8ac77b35730401eabb9fdcc73df3ea28c6c82e55
kkw2758 <dtc3229@gmail.com>
2023-11-24 09:37:42 +0900

    Feat : 시도할 회수를 입력받고 숫자 형식이 아님녀 예외 처리하는 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,4 +1,32 @@
 package racingcar.view;
 
+import racingcar.view.console.ConsoleReader;
+import racingcar.view.console.ConsoleWriter;
+
 public class InputView {
+    private final String NUMBER_REGULAR_EXPRESSION = "\\d+";
+    private final String TRY_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
+    private final String COMMA_SEPARATOR = ",";
+
+    public int requestTryCount() {
+        ConsoleWriter.printlnMessage(TRY_COUNT_REQUEST_MESSAGE);
+        String userInput = ConsoleReader.enterMessage();
+        validateNumber(userInput);
+        return Integer.parseInt(userInput);
+    }
+
+
+    private String[] split(String message, String separator) {
+        return message.split(separator);
+    }
+
+    private void validateNumber(String userInput) {
+        if (isNotNumber(userInput)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNotNumber(String userInput) {
+        return !userInput.matches(NUMBER_REGULAR_EXPRESSION);
+    }
 }
