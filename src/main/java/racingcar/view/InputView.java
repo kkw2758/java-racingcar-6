2ed9b6cfba25925487e422691a71cd0fa374262b
kkw2758 <dtc3229@gmail.com>
2023-11-24 09:38:47 +0900

    Feat : 경주할 자동차 이름을 입력받고 올바른 구분자로 분리 되어 있지 않으면 예외처리한다.

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,10 +1,13 @@
 package racingcar.view;
 
+import java.util.Arrays;
+import java.util.List;
 import racingcar.view.console.ConsoleReader;
 import racingcar.view.console.ConsoleWriter;
 
 public class InputView {
     private final String NUMBER_REGULAR_EXPRESSION = "\\d+";
+    private final String CAR_NAMES_REQUEST_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private final String TRY_COUNT_REQUEST_MESSAGE = "시도할 회수는 몇회인가요?";
     private final String COMMA_SEPARATOR = ",";
 
@@ -15,6 +18,41 @@ public class InputView {
         return Integer.parseInt(userInput);
     }
 
+    public List<String> requestCarNames() {
+        ConsoleWriter.printlnMessage(CAR_NAMES_REQUEST_MESSAGE);
+        String userInput = ConsoleReader.enterMessage();
+        validateCarNamesSeparators(userInput, COMMA_SEPARATOR);
+        return parseStringToList(userInput, COMMA_SEPARATOR);
+    }
+
+    private void validateCarNamesSeparators(String message, String separator) {
+        validateEdgeSeparators(message, separator);
+        validateDuplicatedSeparators(message, separator);
+    }
+
+    private void validateEdgeSeparators(String message, String separator) {
+        if (hasEdgeSeparators(message, separator)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean hasEdgeSeparators(String message, String separator) {
+        return message.startsWith(separator) || message.endsWith(separator);
+    }
+
+    private void validateDuplicatedSeparators(String message, String separator) {
+        if (hasDuplicatedSeparators(message, separator)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean hasDuplicatedSeparators(String message, String separator) {
+        return message.contains(separator.repeat(2));
+    }
+
+    private List<String> parseStringToList(String message, String separator) {
+        return Arrays.stream(split(message, separator)).toList();
+    }
 
     private String[] split(String message, String separator) {
         return message.split(separator);
