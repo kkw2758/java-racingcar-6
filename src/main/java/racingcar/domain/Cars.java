a81565356e564b5288e9311775e38f5fe05fa264
kkw2758 <dtc3229@gmail.com>
2023-11-24 10:29:30 +0900

    Feat : 게임에 참여하는 자동차의 이름이 중복되거나 2대 이상이 아니면 예외 처리 기능 추가

---
+++ src/main/java/racingcar/domain/Cars.java
@@ -0,0 +1,43 @@
+package racingcar.domain;
+
+import java.util.List;
+
+public class Cars {
+    private static final int MIN_CARS_LENGTH = 2;
+    private final List<Car> cars;
+
+    private Cars(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static Cars from(List<String> cars) {
+        return new Cars(cars.stream()
+                .map(Car::from)
+                .toList());
+    }
+
+    private static void validateCars(List<String> cars) {
+        validateCarsDuplicate(cars);
+        validateLength(cars);
+    }
+
+    private static void validateLength(List<String> cars) {
+        if (checkCarsSize(cars)) {
+            throw new IllegalArgumentException("경주에 참여하는 자동차는 2대 이상만 가능합니다.");
+        }
+    }
+
+    private static boolean checkCarsSize(List<String> cars) {
+        return cars.size() < MIN_CARS_LENGTH;
+    }
+
+    private static void validateCarsDuplicate(List<String> cars) {
+        if (checkCarsDuplicate(cars)) {
+            throw new IllegalArgumentException("게임에 참여하는 자동차의 이름이 중복될 수 없습니다.");
+        }
+    }
+
+    private static boolean checkCarsDuplicate(List<String> cars) {
+        return cars.stream().distinct().count() != cars.size();
+    }
+}
